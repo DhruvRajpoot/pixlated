334dbac9e5497c5c89ed30ab8e27df586946f23f
Bridgetamana <amanabridget03@gmail.com>
2025-11-07 17:16:10 +0100

    improved descriptions and code examples

--- src/pixlated-bg.js
+++ src/pixlated-bg.js
@@ -1,22 +1,22 @@
 import { applyNoise, clampIntensity } from './utils/noise.js';
 
 class PixlatedBg extends HTMLElement {
-  static DEBUG = false;
+    static DEBUG = false;
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
 
-    this.canvas = document.createElement('canvas');
-    this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
 
-    if (!this.ctx) {
-      console.error('<pixlated-bg>: Canvas 2D context not supported');
-      return;
-    }
+        if (!this.ctx) {
+            console.error('<pixlated-bg>: Canvas 2D context not supported');
+            return;
+        }
 
-    this.styleEl = document.createElement('style');
-    this.styleEl.textContent = `
+        this.styleEl = document.createElement('style');
+        this.styleEl.textContent = `
       :host {
         display: block;
         position: relative;
@@ -36,107 +36,107 @@ class PixlatedBg extends HTMLElement {
       }
     `;
 
-    this.contentSlot = document.createElement('div');
-    this.contentSlot.className = 'content';
-    this.contentSlot.innerHTML = '<slot></slot>';
+        this.contentSlot = document.createElement('div');
+        this.contentSlot.className = 'content';
+        this.contentSlot.innerHTML = '<slot></slot>';
 
-    this.shadowRoot.append(this.styleEl, this.canvas, this.contentSlot);
-  }
+        this.shadowRoot.append(this.styleEl, this.canvas, this.contentSlot);
+    }
 
-  static get observedAttributes() {
-    return ['color', 'intensity', 'width', 'height'];
-  }
+    static get observedAttributes() {
+        return ['color', 'intensity', 'width', 'height'];
+    }
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue === newValue) return;
-    
-    switch (name) {
-      case 'color':
-      case 'intensity':
-        if (this.isConnected) this.drawGrainyBackground();
-        break;
-      case 'width':
-      case 'height':
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) return;
+
+        switch (name) {
+            case 'color':
+            case 'intensity':
+                if (this.isConnected) this.drawGrainyBackground();
+                break;
+            case 'width':
+            case 'height':
+                this.updateCanvasSize();
+                if (this.isConnected) this.drawGrainyBackground();
+                break;
+        }
+    }
+
+    connectedCallback() {
         this.updateCanvasSize();
-        if (this.isConnected) this.drawGrainyBackground();
-        break;
+        this.drawGrainyBackground();
+
+        this.resizeObserver = new ResizeObserver(() => {
+            this.updateCanvasSize();
+            this.drawGrainyBackground();
+        });
+        this.resizeObserver.observe(this);
     }
-  }
 
-  connectedCallback() {
-    this.updateCanvasSize();
-    this.drawGrainyBackground();
-    
-    this.resizeObserver = new ResizeObserver(() => {
-      this.updateCanvasSize();
-      this.drawGrainyBackground();
-    });
-    this.resizeObserver.observe(this);
-  }
-
-  disconnectedCallback() {
-    if (this.resizeObserver) {
-      this.resizeObserver.disconnect();
+    disconnectedCallback() {
+        if (this.resizeObserver) {
+            this.resizeObserver.disconnect();
+        }
     }
-  }
 
-  updateCanvasSize() {
-    const width = this.getAttribute('width');
-    const height = this.getAttribute('height');
-    
-    if (width && height) {
-      const w = parseInt(width, 10);
-      const h = parseInt(height, 10);
-      const dpr = window.devicePixelRatio || 1;
-      
-      this.canvas.width = w * dpr;
-      this.canvas.height = h * dpr;
-      this.canvas.style.width = `${w}px`;
-      this.canvas.style.height = `${h}px`;
-      this.ctx.scale(dpr, dpr);
-    } else {
-      const rect = this.getBoundingClientRect();
-      const dpr = window.devicePixelRatio || 1;
-      
-      this.canvas.width = rect.width * dpr;
-      this.canvas.height = rect.height * dpr;
-      this.ctx.scale(dpr, dpr);
+    updateCanvasSize() {
+        const width = this.getAttribute('width');
+        const height = this.getAttribute('height');
+
+        if (width && height) {
+            const w = parseInt(width, 10);
+            const h = parseInt(height, 10);
+            const dpr = window.devicePixelRatio || 1;
+
+            this.canvas.width = w * dpr;
+            this.canvas.height = h * dpr;
+            this.canvas.style.width = `${w}px`;
+            this.canvas.style.height = `${h}px`;
+            this.ctx.scale(dpr, dpr);
+        } else {
+            const rect = this.getBoundingClientRect();
+            const dpr = window.devicePixelRatio || 1;
+
+            this.canvas.width = rect.width * dpr;
+            this.canvas.height = rect.height * dpr;
+            this.ctx.scale(dpr, dpr);
+        }
     }
-  }
 
-  drawGrainyBackground() {
-    if (!this.ctx) return;
+    drawGrainyBackground() {
+        if (!this.ctx) return;
 
-    const color = this.getAttribute('color') || '#09090b';
-    const rawIntensity = parseFloat(this.getAttribute('intensity'));
-    const intensity = clampIntensity(rawIntensity, 0.1);
-    
-    const canvasWidth = this.canvas.width;
-    const canvasHeight = this.canvas.height;
+        const color = this.getAttribute('color') || '#09090b';
+        const rawIntensity = parseFloat(this.getAttribute('intensity'));
+        const intensity = clampIntensity(rawIntensity, 0.1);
 
-    this.ctx.fillStyle = color;
-    this.ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        const canvasWidth = this.canvas.width;
+        const canvasHeight = this.canvas.height;
 
-    if (intensity > 0) {
-      const imageData = this.ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-      const noisyData = applyNoise(imageData, intensity);
-      this.ctx.putImageData(noisyData, 0, 0);
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        if (intensity > 0) {
+            const imageData = this.ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+            const noisyData = applyNoise(imageData, intensity);
+            this.ctx.putImageData(noisyData, 0, 0);
+        }
     }
-  }
 
-  reload() {
-    this.drawGrainyBackground();
-  }
+    reload() {
+        this.drawGrainyBackground();
+    }
 
-  getConfig() {
-    const rawIntensity = parseFloat(this.getAttribute('intensity'));
-    return {
-      color: this.getAttribute('color') || '#09090b',
-      intensity: clampIntensity(rawIntensity, 0.1),
-      width: this.canvas.width,
-      height: this.canvas.height
-    };
-  }
+    getConfig() {
+        const rawIntensity = parseFloat(this.getAttribute('intensity'));
+        return {
+            color: this.getAttribute('color') || '#09090b',
+            intensity: clampIntensity(rawIntensity, 0.1),
+            width: this.canvas.width,
+            height: this.canvas.height
+        };
+    }
 }
 
 customElements.define('pixlated-bg', PixlatedBg);
